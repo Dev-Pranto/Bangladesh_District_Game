67285ea2106cd00c94dc05d86a9283fc99413324
SK Hamim Ishthiaqe Pranto <hamimistiak22@gmail.com>
2024-03-26 14:42:31 +0600

    2nd

--- main.py
+++ main.py
@@ -17,11 +17,10 @@ my_pen.hideturtle()
 my_pen.penup()
 
 data = pandas.read_csv('district_list.csv')
-district = data['District_Name'].tolist()
+district_list = data['District_Name'].tolist()
 xcor = data['X'].tolist()
 ycor = data['Y'].tolist()
 guessed_district = []
-missed_district = []
 
 
 condition = True
@@ -29,9 +28,9 @@ count = 0
 while condition:
     user_input = sc.textinput(title=f'{count}/64 District correct',
                               prompt="What's the another District name: ")
-    for item in district:
+    for item in district_list:
         if item == user_input.title():
-            index = district.index(item)
+            index = district_list.index(item)
             my_pen.goto(x=xcor[index] - 20, y=ycor[index])
             my_pen.write(arg=item, move=False, font=('Arial', 8, 'normal'))
             guessed_district.append(item)
@@ -39,7 +38,5 @@ while condition:
 
     if count > 64 or user_input.title() == 'Exit':
         condition = False
-        for item in district:
-            if item not in guessed_district:
-                missed_district.append(item)
+        missed_district = [districts for districts in district_list if districts not in guessed_district]
         pandas.DataFrame(missed_district).to_csv('district_to_learn.csv')
